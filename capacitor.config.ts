7f14d1db72df9cd24017d149e2240103153b506f
Alone-07 <al71one2002@gmail.com>
2025-01-17 15:19:57 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'penZ',
+  webDir: 'www'
+};
+
+export default config;
