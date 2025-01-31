502ad63fd6f99a86b29c22869cca59454675b418
Ayyanraj <ayyanayyan0060@gmail.com>
2025-01-31 18:20:00 +0530

    add goals rework commit

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,16 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'penZ',
-  webDir: 'www'
+  webDir: 'www',
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 3000,
+      launchAutoHide: false,
+      androidScaleType: "CENTER_CROP",
+      backgroundColor: "#ffffffff",
+      androidSplashResourceName: "splash",
+    },
+  }
 };
 
 export default config;
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { register } from 'swiper/element/bundle';
 import { Platform } from '@ionic/angular';
 import { StatusBarService } from './services/status-bar';
+import {SplashScreen} from '@capacitor/splash-screen';
+import { Capacitor } from '@capacitor/core';
 register();
 
 @Component({
@@ -15,6 +17,11 @@ export class AppComponent {
     private platform: Platform,
     private statusBarService: StatusBarService,
   ) {
+    SplashScreen.hide();
+    SplashScreen.show({
+      showDuration: 2000,
+      autoHide: true,
+    });
     this.initializeApp();
   }
 
