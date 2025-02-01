3f0153a184e1d89e0d6a3a8e051827d441cb975b
Ayyanraj <ayyanayyan0060@gmail.com>
2025-02-01 18:55:38 +0530

    share popup added.

--- src/app/products/products.page.ts
+++ src/app/products/products.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Share } from '@capacitor/share';
+import {  NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-products',
@@ -11,8 +13,14 @@ export class ProductsPage implements OnInit {
   rating: number = 0;
   stars: number[] = [1, 2, 3, 4, 5];
   isFavorite: boolean = false;
+  
+  private productDetails = {
+    title: 'Clinique Moisture Surge 100H Auto-Replenishing Hydrator',
+    description: 'Refreshing oil-free face moisturizer with aloe bioferment and hyaluronic acid delivers instant hydration that soothes in 3 seconds. 100% instantly show a boost in hydration and glow.',
+    imageUrl: '../../assets/images/prod.png'
+  };
 
-  constructor() { }
+  constructor(private navigator: NavController) { }
 
   ngOnInit() { }
 
@@ -22,5 +30,22 @@ export class ProductsPage implements OnInit {
 
   toggleFavorite() {
     this.isFavorite = !this.isFavorite;
+  }
+
+  async shareProduct() {
+    try {
+      await Share.share({
+        title: this.productDetails.title,
+        text: this.productDetails.description,
+        url: window.location.href,
+        dialogTitle: 'Share Product Details'
+      });
+    } catch (error) {
+      console.error('Error sharing product:', error);
+    }
+  }
+
+  public goBack(): void {
+    this.navigator.back();
   }
 }
